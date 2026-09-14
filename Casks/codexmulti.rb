2c8ccca6afb41619966a7ce637078405bf7a6ef4
Moonsun Kim <fazomo@tublet.io>
2026-09-14 14:21:09 +0900

    Update CodexMulti to 0.2.10

--- Casks/codexmulti.rb
+++ Casks/codexmulti.rb
@@ -1,6 +1,6 @@
 cask "codexmulti" do
-  version "0.2.9"
-  sha256 "3f0a22a8b0c0ab5489cb397eccc594179c8b1a59cf9e5611e0ca40f6204ed8bc"
+  version "0.2.10"
+  sha256 "74321a39b4b0973a947fa5b2dd15e7b6aec1b712fb3c58259bd5f1fc6cecf5d1"
 
   url "https://github.com/moonsunkim/codexmulti/releases/download/v#{version}/CodexMulti-#{version}.zip"
   name "CodexMulti"
